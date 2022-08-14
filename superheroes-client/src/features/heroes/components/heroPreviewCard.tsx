d7b61db2162d2c2241811599141de000cd3df7df
dmytr <dmitriu564@gmail.com>
2022-08-14 17:21:34 +0300

    Front-end completed. Supports loading and uploading images. Corrected readme.

--- superheroes-client/src/features/heroes/components/heroPreviewCard.tsx
+++ superheroes-client/src/features/heroes/components/heroPreviewCard.tsx
@@ -13,22 +13,31 @@ const Container = styled.div`
     grid-template-columns: 9fr minmax(min-content, 1fr);
     grid-template-rows: minmax(min-content, auto) 1.5fr;
 
-    border: 2px solid palegoldenrod;
+    border: 2px none;
     border-radius: 5px;
     padding: 10px 10px;
     margin-bottom: 10px;
+
+    background: lightblue;
+
+    &:hover{
+      background: #94cde0;
+    }
 `;
 
 const RemoveButton = styled.button`
     grid-column: 2;
     grid-row:  1;
     justify-self: end;
+    align-self: center;
 
-    background: white;
-    color: red; 
+    max-height: 0.5fr;
+
+    background: red;
+    color: white; 
     font-size: 1em; 
-    padding: 0.5em 0.5em;   
-    border: 2px solid red;
+    padding: 0.5em;   
+    border: none;
     border-radius: 5px;
 
     &:hover {
@@ -36,8 +45,8 @@ const RemoveButton = styled.button`
     }
 
     &:active {
-        background: red;
-        color: white;
+        background: darkred;
+        color: gray;
     }
 `;
 
@@ -45,8 +54,8 @@ const HeroPreviewImage = styled.img`
     grid-column: 1;
     grid-row:  2;
 
-    max-width: 100%;
-    max-height:100%;
+    max-width: 200px;
+    max-height:200px;
 `;
 
 const NickNameLabel = styled.span`
@@ -64,7 +73,7 @@ export const HeroPreviewCard = ({ hero, onClick }: HeroCardProps) => {
     <Container onClick={onClick}>
       <NickNameLabel>{hero.nickname}</NickNameLabel>
       <RemoveButton onClick={handleRemove}>x</RemoveButton>
-      <HeroPreviewImage src={hero.image_paths && `${process.env.REACT_APP_BACKEND_ADDRESS}/images/${hero.image_paths[0]}`} alt="Hero preview image" />
+      {hero.image_paths && !!hero.image_paths.length && <HeroPreviewImage src={`${process.env.REACT_APP_BACKEND_ADDRESS}/images/${hero.image_paths[0]}`} alt="Hero preview image" />}
     </Container>
   );
 };
--- superheroes-client/src/features/heroes/heroesAPI.ts
+++ superheroes-client/src/features/heroes/heroesAPI.ts
@@ -3,3 +3,7 @@ import axios from 'axios';
 export const fetchHeroes = async (size: number, offset: number) => (await axios.get('/heroes', { params: { size, offset } })).data;
 
 export const deleteHeroById = async (id: string) => (await axios.delete(`/heroes/${id}`)).data;
+
+export const createHero = async (heroData: FormData) => (await axios.post('/heroes', heroData)).data;
+
+export const editHero = async (heroData: FormData) => (await axios.patch('/heroes', heroData)).data;
